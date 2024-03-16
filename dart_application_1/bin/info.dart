bccadd4e577b290d0eee7f235a0ee52aadae7a71
Celyne <dianakydd2@gmail.com>
2024-03-16 12:05:18 +0300

    Created info.dart to display personal info

---
+++ dart_application_1/bin/info.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Celyne";
+  int age = 28;
+  String school = "PLP";
+  String hobby = "Reading";
+
+  print(
+      "My name is $name, I am $age years old, I go to $school, and I enjoy $hobby.");
+}
